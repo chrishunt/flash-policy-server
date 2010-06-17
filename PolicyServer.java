2389c8ef8b47722456aecc398742e48306a78845
Chris Hunt <huntca@gmail.com>
2010-06-17 13:46:54 -0700

    Initial Commit

---
+++ PolicyServer.java
@@ -0,0 +1,42 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PolicyServer {
+  public static void main (String args[]) {
+    final int DEFAULT_PORT = 1234;
+
+    // Set server port
+    int port = DEFAULT_PORT;
+    try {
+      port = Integer.parseInt(args[0]);
+    } catch (Exception e){
+      port = DEFAULT_PORT;
+    }
+
+    // Start server socket
+    ServerSocket serverSocket = null;
+    try {
+      serverSocket = new ServerSocket(port);
+      System.out.println("Server listening on port: " + port);
+    } catch (IOException e) {
+      System.err.println("Could not listen on port: " + port);
+      System.exit(1);
+    }
+
+    while (true) {
+      // Accept a client and spawn a thread
+      Socket clientSocket = null;
+      try {
+        clientSocket = serverSocket.accept();
+        System.out.println("Connection accepted, sending XML...");
+      } catch (IOException e) {
+        System.out.println("Accept failed: " + port);
+      }
+
+      SecurityThread thread = new SecurityThread(clientSocket);
+      thread.run();
+    }
+  }
+}
---
+++ SecurityThread.java
@@ -0,0 +1,38 @@
+import java.net.Socket;
+import java.io.PrintWriter;
+import java.io.IOException;
+
+public class SecurityThread extends Thread {
+  private Socket clientSocket = null;
+
+  public SecurityThread (Socket clientSocket){
+    this.clientSocket = clientSocket;
+  }
+
+  public void run(){
+    // Create an new output stream with auto-flush enabled
+    PrintWriter out = null;
+    try {
+      out = new PrintWriter(clientSocket.getOutputStream(), true);
+    } catch (IOException e) {
+      System.err.println("Could not create output stream for client");
+    }
+
+    // Send our 'allow all' security policy to the flash client
+    String policy = 
+      "<cross-domain-policy>" +
+        "<allow-access-from domain=\"*\" to-ports=\"*\" />" +
+      "</cross-domain-policy>";
+
+    out.println(policy);
+
+    
+    // Clost this client socket
+    try {
+      out.close();
+      clientSocket.close();
+    } catch (IOException e) {
+      System.err.println("Could not close client socket.");
+    }
+  }
+}
